a10784d20b21230896562914e6c70f4401999410
CarlosFBrandao <carlosbrand.machado@gmail.com>
2023-07-22 19:20:11 -0300

    Conclusão do projeto pokedex

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+
+  private urlPokemon = 'https://pokeapi.co/api/v2/pokemon/'
+  private urlPokemonNames = 'https://pokeapi.co/api/v2/pokemon-species/'
+
+  public pokemon: any;
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
+  constructor(
+    private activedRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ){}
+
+  ngOnInit(): void {
+    this.getPokemon();
+  }
+
+
+  public getPokemon(){
+    const id = this.activedRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}${id}`);
+    const name = this.pokeApiService.apiGetPokemon(`${this.urlPokemonNames}${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error =>{
+        this.apiError = true
+      }
+      
+    );
+  }
 
 }
